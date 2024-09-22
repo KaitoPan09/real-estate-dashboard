863b2d27adfb601c066c70d450128c0d65bd0b01
KaitoPan09 <93980610+KaitoPan09@users.noreply.github.com>
2024-09-22 21:27:17 +0800

    detailed view layout
    1. drawer and card component to detail view
    todo:
    1. add pictures
    2. add more details

--- real-estate-dashboard/src/dashboard/components/property-lists.jsx
+++ real-estate-dashboard/src/dashboard/components/property-lists.jsx
@@ -1,5 +1,32 @@
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { FaBed } from "react-icons/fa";
+import { SlSizeFullscreen } from "react-icons/sl";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const PropertyList = ({ properties }) => {
   return (
@@ -41,9 +68,72 @@ const PropertyList = ({ properties }) => {
                 $ {property.price}
               </Button>
             </div>
-            <Button variant="outline" size="lg">
+            <Drawer>
+              <DrawerTrigger>
+                <Button variant="outline" size="lg">
+                  <Plus /> View Property
+                </Button>
+              </DrawerTrigger>
+              <DrawerContent>
+                <DrawerHeader>
+                  <DrawerTitle>
+                    {property.name} - {property.occupancy}
+                  </DrawerTitle>
+                  <div>{property.location}</div>
+                  <DrawerDescription>{property.description}</DrawerDescription>
+                  <div className="flex items-center text-2xl">
+                    $ {property.price}
+                  </div>
+                  <div className="flex items-center justify-center">
+                    <Carousel
+                      opts={{
+                        align: "start",
+                      }}
+                      className="w-full max-w-xs sm:max-w-5xl"
+                    >
+                      <CarouselContent>
+                        {Array.from({ length: 5 }).map((_, index) => (
+                          <CarouselItem
+                            key={index}
+                            className="md:basis-1/2 lg:basis-1/3"
+                          >
+                            <div className="p-1">
+                              <Card>
+                                <CardContent className="flex aspect-square items-center justify-center p-6">
+                                  <span className="text-3xl font-semibold">
+                                    {index + 1}
+                                  </span>
+                                </CardContent>
+                              </Card>
+                            </div>
+                          </CarouselItem>
+                        ))}
+                      </CarouselContent>
+                      <CarouselPrevious />
+                      <CarouselNext />
+                    </Carousel>
+                  </div>
+                  <div className="flex items-center justify-center">
+                    <div className="flex items-center text-xl mr-5">
+                      <FaBed className="mr-1" />
+                      <span>{property.number_of_rooms}</span>
+                    </div>
+                    <div className="flex items-center text-xl">
+                      <SlSizeFullscreen className="mr-1" />
+                      <span>{property.size}</span>
+                    </div>
+                  </div>
+                </DrawerHeader>
+                <DrawerFooter>
+                  <DrawerClose>
+                    <Button variant="outline">Close</Button>
+                  </DrawerClose>
+                </DrawerFooter>
+              </DrawerContent>
+            </Drawer>
+            {/* <Button variant="outline" size="lg">
               <Plus /> View Property
-            </Button>
+            </Button> */}
           </div>
         </li>
       ))}
